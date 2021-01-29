eab980611123d5ec520c65f435eac6388c279274
Marcin <mwoz123@git>
2021-01-29 16:48:43 +0100

    init comit

---
+++ src/index.js
@@ -0,0 +1,23 @@
+function random_bg_color() {
+    var x = Math.floor(Math.random() * 256);
+    var y = Math.floor(Math.random() * 256);
+    var z = Math.floor(Math.random() * 256);
+    var bgColor = "rgb(" + x + "," + y + "," + z + ")";
+    return bgColor;
+}
+
+function changeColor() {
+    // var x = document.querySelector('div');
+    var x = $('div');
+    x.style.width = "100px";
+    x.style.height = "100px"
+    x.style.backgroundColor =random_bg_color();
+}
+
+function changeColorJQ(params) {
+    var x = $('div')
+    x.css('width', '101px')
+    x.css('height', '100px')
+    x.css('backgroundColor', random_bg_color());
+    
+}
---
+++ src/test2.js
@@ -0,0 +1,7 @@
+var a = test();
+function bbbb(params) {
+    
+}
+
+// document.querySelector('button').addEventListener('click',changeColor)
+$('button').on('click', changeColorJQ)
